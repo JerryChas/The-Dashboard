e1887b51599c5f1cd42cd333ef8feb27a0ab5089
jerryChas <jerry.olsson@chasacademy.se>
2023-12-26 09:52:54 +0100

    created textarea

---
+++ js/note.js
@@ -0,0 +1,8 @@
+// Hämtar container till note
+const noteContainer = document.querySelector('.note_container');
+
+// Skapa en textarea-element
+const noteArea = document.createElement('textarea');
+
+// Lägg till textarea i noteContainer
+noteContainer.appendChild(noteArea);
