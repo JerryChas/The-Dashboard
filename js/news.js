54bb54154667bc80199817caa8f7dfd87691f24c
jerryChas <jerry.olsson@chasacademy.se>
2023-12-26 15:27:33 +0100

    latest news shows in card

--- js/news.js
+++ js/news.js
@@ -1,4 +1,5 @@
-const newsArray = []; // En tom array där vi kommer att lagra objekt för varje nyhetsartikel.
+// En tom array där vi kommer att lagra objekt för varje nyhetsartikel.
+const newsArray = [];
 
 // Hämtar container till nyhetsflödet
 const newsContainer = document.querySelector('.news_container');
@@ -6,47 +7,54 @@ const newsContainer = document.querySelector('.news_container');
 //* ----===----===----=== FUNKTIONER ===----===----===---- *//
 
 //* ----=== HÄMTAR SENASTE NYHETER
-function fetchLatestNews() {
-    // Hämtar data från RSS-flödet
-    fetch('https://rss.aftonbladet.se/rss2/small/pages/sections/senastenytt/')
-        .then(response => response.text())
-        .then(data => {
-            // Skapar en XML-parser för att behandla XML-data
-            const parser = new DOMParser();
-            const xmlDoc = parser.parseFromString(data, 'application/xml');
+async function fetchLatestNews() {
+    try {
+        // Hämtar data från RSS-flödet
+        const response = await fetch('https://rss.aftonbladet.se/rss2/small/pages/sections/senastenytt/');
+        const data = await response.text();
 
-            // Nu har du xmlDoc, och du kan arbeta med det här.
-            const items = xmlDoc.querySelectorAll('item');
+        // Skapar en XML-parser för att behandla XML-data
+        const parser = new DOMParser();
+        const xmlDoc = parser.parseFromString(data, 'application/xml');
 
-            // Loopar igenom varje item i RSS-flödet
-            items.forEach(item => {
-                // Hämtar information från varje item och skapar ett objekt.
-                const newsObject = {
-                    title: item.querySelector('title').textContent,
-                    link: item.querySelector('link').textContent,
-                    // Description är omgiven av CDATA-sektion och behöver därför hanteras
-                    description: handleCDATAsection(item.querySelector('description')),
-                    pubDate: item.querySelector('pubDate').textContent,
-                    imageUrl: item.querySelector('enclosure') ? item.querySelector('enclosure').getAttribute('url') : null,
-                };
+        // Nu har du xmlDoc, och du kan arbeta med det här.
+        const items = xmlDoc.querySelectorAll('item');
 
-                // Lägger till det skapade objektet i arrayen.
-                newsArray.push(newsObject);
-            });
+        // Loopar igenom varje item i RSS-flödet
+        items.forEach(item => {
+            // Hämtar information från varje item och skapar ett objekt.
+            const newsObject = {
+                title: item.querySelector('title').textContent,
+                link: item.querySelector('link').textContent,
+                // Description är omgiven av CDATA-sektion och behöver därför hanteras
+                description: handleCDATAsection(item.querySelector('description')),
+                pubDate: item.querySelector('pubDate').textContent,
+                imageUrl: item.querySelector('enclosure') ? item.querySelector('enclosure').getAttribute('url') : null,
+            };
 
-            // Nu har du en array med objekt för varje item i RSS-flödet.
-            console.log(newsArray);
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
+            // Lägger till det skapade objektet i arrayen.
+            newsArray.push(newsObject);
         });
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
 }
 
 //* ----=== RENDERAR NYHETSFLÖDE
 function renderLatestNews() {
-    
-    
-    
+    const newsHTML = newsArray.map((obj) => {
+
+        // Returnera HTML
+        return `
+        <div class="news card-items">
+          <a href="${obj.link}" target="_blank" class="item_target">
+            <h3>${obj.title}</h3>
+          </a>
+        </div>
+      `;
+    });
+
+    newsContainer.innerHTML = newsHTML.join('');
 }
 
 //* ----=== Hanterar CDATA-sektion
@@ -58,4 +66,5 @@ function handleCDATAsection(element) {
 //* ----===----===----=== ===----=== ===----===----===---- *//
 
 // Anropar funktionen för att hämta och behandla nyhetsdata.
-fetchLatestNews(); 
+fetchLatestNews()
+.then(renderLatestNews);
