cad7fa2d7cad9a6657e3770239bae25a4099ff1e
jerryChas <jerry.olsson@chasacademy.se>
2023-12-26 10:16:33 +0100

    notes saves data in localstorage

--- js/note.js
+++ js/note.js
@@ -2,7 +2,17 @@
 const noteContainer = document.querySelector('.note_container');
 
 // Skapa en textarea-element
-const noteArea = document.createElement('textarea');
+const noteArea = document.createElement('textarea')
+noteArea.setAttribute('placeholder', 'Skriv dina tankar här')
 
 // Lägg till textarea i noteContainer
 noteContainer.appendChild(noteArea);
+
+
+//* LOCAL STORAGE *//
+// Hämtar anteckningar från localstorage
+noteArea.value = JSON.parse(localStorage.getItem('Notes'))
+// lagrar anteckningarna i localstorage varje gång det skrivs
+noteArea.addEventListener('input', () => {
+    localStorage.setItem('Notes', JSON.stringify(noteArea.value))
+})
